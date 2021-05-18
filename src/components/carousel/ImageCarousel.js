ca6ff08c91d9b442d80e9629130d407b8e8f8ffd
Saksham Mathur <saksham.mathur25@gmail.com>
2021-05-19 00:45:35 +0530

    Added homepage images

--- src/components/carousel/ImageCarousel.js
+++ src/components/carousel/ImageCarousel.js
@@ -1,38 +1,46 @@
 const ImageCarousel = () => {
-    return (
-        <div>
-            <div
-                id="carouselExampleIndicators"
-                className="carousel slide"
-                data-bs-ride="carousel"
-            >
-                <div className="carousel-indicators">
-                    <button
-                        type="button"
-                        data-bs-target="#carouselExampleIndicators"
-                        data-bs-slide-to="0"
-                        className="active"
-                        aria-current="true"
-                        aria-label="Slide 1"
-                    />
-                    <button
-                        type="button"
-                        data-bs-target="#carouselExampleIndicators"
-                        data-bs-slide-to="1"
-                        aria-label="Slide 2"
-                    />
-                </div>
-                <div className="carousel-inner">
-                    <div className="carousel-item active">
-                        <img src="" className="d-block w-100" alt="Banner 1"/>
-                    </div>
-                    <div className="carousel-item">
-                        <img src="" className="d-block w-100" alt="Banner 2"/>
-                    </div>
-                </div>
-            </div>
+  return (
+    <div>
+      <div
+        id="carouselExampleIndicators"
+        className="carousel slide"
+        data-bs-ride="carousel"
+      >
+        <div className="carousel-indicators">
+          <button
+            type="button"
+            data-bs-target="#carouselExampleIndicators"
+            data-bs-slide-to="0"
+            className="active"
+            aria-current="true"
+            aria-label="Slide 1"
+          />
+          <button
+            type="button"
+            data-bs-target="#carouselExampleIndicators"
+            data-bs-slide-to="1"
+            aria-label="Slide 2"
+          />
         </div>
-    );
+        <div className="carousel-inner">
+          <div className="carousel-item active">
+            <img
+              src="/images/banner1.jpg"
+              className="d-block w-100"
+              alt="Banner 1"
+            />
+          </div>
+          <div className="carousel-item">
+            <img
+              src="/images/banner2.jpg"
+              className="d-block w-100"
+              alt="Banner 2"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ImageCarousel;
